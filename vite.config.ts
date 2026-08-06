8aab5364296ef751987230269dcccd5c8682d204
Shadowrunner11 <pieroramirez810@gmail.com>
2026-08-05 20:42:24 -0500

    example: vitest storybook run

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vitest/config';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { vitestWebContainers } from "@webcontainer/test/plugin";
+import { preview }from '@vitest/browser-preview'
 
 export default defineConfig({
+  plugins: [
+    // vitestWebContainers(),
+    // storybookTest({
+    //   configDir: ".storybook",
+    // }),
+  ],
   test: {
-    testTimeout: 4000, 
+    testTimeout: 3000, 
+    // include: ["./examples/**/*.stories.?(c|m)[jt]s?(x)"],
+    // browser: {
+    //   enabled: true,
+    //   name: "chromium",
+    //   provider: preview(),
+    //   instances: [
+    //     { browser: "chromium" }
+    //   ]
+    // },
   },
 });
