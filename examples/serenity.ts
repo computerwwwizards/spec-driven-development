e9484687d28539d7651fe7395ee28214b9798a39
Shadowrunner11 <pieroramirez810@gmail.com>
2026-08-06 11:42:13 -0500

    package: installed serenety

---
+++ examples/serenity.ts
@@ -0,0 +1,6 @@
+import { PageElement, Selector, By } from '@serenity-js/web';
+
+//TODO: implement a full storybook adapter to use as a brwser ability
+class StorybookPageElement extends PageElement<HTMLElement>{
+
+}
